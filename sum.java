105f99e0d3e5b6259f480bad7fb531f4253d6cd6
root <root@jenkins.cspl.com>
2019-05-14 12:03:47 +0530

    This is the java code

---
+++ sum.java
@@ -0,0 +1,17 @@
+   import java.util.Scanner;
+     
+    class AddNumbers
+    {
+       public static void main(String args[])
+       {
+          int x, y, z;
+     
+          System.out.println("Enter two integers to calculate their sum");
+         
+          x = 25
+          y = 43
+          z = x + y;
+         
+          System.out.println("Sum of the integers = " + z);
+       }
+    }
